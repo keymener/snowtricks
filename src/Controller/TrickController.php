c4f64bba16ed3b43b999b77100e87cdaa26179f6
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2019-01-08 23:32:55 +0100

    entity Trick created

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use App\Form\TrickType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrickController extends AbstractController
+{
+
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+
+    /**
+     * @Route("/trick/new", name="trick_new")
+     */
+    public function newTrick(Request $request)
+    {
+
+        $trick = new Trick();
+
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+
+            $trick->setDate(new \DateTime());
+
+            $this->em->persist($trick);
+            $this->em->flush();
+
+
+            $this->addFlash('success', 'La figure a bien été enregistrée');
+
+        }
+
+        return $this->render('trick/newTrick.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
