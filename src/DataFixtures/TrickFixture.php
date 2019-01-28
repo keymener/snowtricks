4fe5fd3c1e869a6ea3a9ded5c691c08ee770646a
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2019-01-28 22:48:08 +0100

    add some lorem into fixtures*

--- src/DataFixtures/TrickFixture.php
+++ src/DataFixtures/TrickFixture.php
@@ -24,6 +24,14 @@ class TrickFixture extends Fixture
             $trick = new Trick();
             $trick->setName('Figure' . $i);
             $trick->setDate(new \DateTime());
+            $trick->setDescription('
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+               mollit anim id est laborum.
+            
+            ');
 
             $group = new TrickGroup();
             $group->setName('Mon groupe ' . $i);
