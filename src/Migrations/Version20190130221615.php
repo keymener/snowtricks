5faf7347f86b972234ef5ee1f546ec6ecc2458b9
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2019-01-30 23:17:32 +0100

    hotfix database updtate

---
+++ src/Migrations/Version20190130221615.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190130221615 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE image DROP is_first');
+        $this->addSql('ALTER TABLE trick ADD first_image_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE trick ADD CONSTRAINT FK_D8F0A91E89BD1736 FOREIGN KEY (first_image_id) REFERENCES image (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D8F0A91E89BD1736 ON trick (first_image_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE image ADD is_first TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE trick DROP FOREIGN KEY FK_D8F0A91E89BD1736');
+        $this->addSql('DROP INDEX UNIQ_D8F0A91E89BD1736 ON trick');
+        $this->addSql('ALTER TABLE trick DROP first_image_id');
+    }
+}
