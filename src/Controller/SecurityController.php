a865754728045702fefe17862139ca1475ff0a31
Keigo Matsunaga <keigo.matsunaga@gmail.com>
2019-02-15 23:01:49 +0100

    login interface

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,7 @@ use App\Form\UserRegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -30,37 +31,6 @@ class SecurityController extends AbstractController
     public function __construct(EntityManagerInterface $em)
     {
         $this->em = $em;
-    }
-
-
-    /**
-     * @Route("/login", name="user_login")
-     */
-    public function login(Request $request, AuthenticationUtils $authUtils)
-    {
-
-        $error = $authUtils->getLastAuthenticationError();
-
-        $lastUsername = $authUtils->getLastUsername();
-
-        $form = $this->createForm(UserRegistrationType::class);
-
-
-        return $this->render('security/login.html.twig', [
-            'form' => $form->createView(),
-            'lastUsername' => $lastUsername,
-            'error' => $error,
-        ]);
-
-    }
-
-
-    /**
-     * @Route("/logout", name="logout")
-     */
-    public function logout()
-    {
-
     }
 
 
@@ -97,6 +67,27 @@ class SecurityController extends AbstractController
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
         ]);
+
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
 
     }
 
